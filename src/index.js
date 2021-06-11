eac68e633b5e7f842bfa3fed65f89bb6c9f9d889
brocco <saaristizabalco@gmail.com>
2021-06-11 15:00:49 -0500

    ESlint

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './components/HelloWorld';
+import App from '../public/index.html' 
+
+
+
+
+ReactDOM.render(<HelloWorld />, document.getElementById('App'));
